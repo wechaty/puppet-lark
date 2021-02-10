3aa2c8304d9f78a7747c1f03488ac84e2121272a
Roxanne718 <953299708@qq.com>
2021-02-10 16:00:14 +0800

    replace console with log.

--- src/puppet-lark.ts
+++ src/puppet-lark.ts
@@ -13,6 +13,7 @@ import {
   UrlLinkPayload,
   FileBox,
   EventDongPayload,
+  log,
 } from 'wechaty-puppet'
 
 import { VERSION } from './version'
@@ -65,7 +66,6 @@ class PuppetLark extends Puppet {
     const _port = this.options.larkServer?.port ? this.options.larkServer?.port : 1234
 
     if (this.appId === null || this.appSecret === null || this.appVerificationToken === null) {
-      console.info('ERROR: Please add environment variable.')
       throw new Error('Environment variable not found.')
     }
 
@@ -76,8 +76,7 @@ class PuppetLark extends Puppet {
         port: listenedPort,
         subdomain: 'wechaty-puppet-lark',
       })
-      console.info('Server is running on ', this.localTunnel.url, ' now')
-      console.info('Please verify it on your lark bot and app.')
+      log.info('Server is running on ', this.localTunnel.url, ' now.\nPlease verify it on your lark bot and app.')
     })
 
     this.id = this.appId
@@ -87,15 +86,15 @@ class PuppetLark extends Puppet {
       const payload = req.body
       // verify token
       if (payload.token !== this.appVerificationToken) {
-        console.info('verification token not match, token =', payload)
+        console.error('verification token not match, token = ', payload)
+        log.error('verification token not match, token = ', payload)
         return null
       }
       // response according to message type
       if (payload.type === 'url_verification') {
         res.status(200).json({ challenge: payload.challenge })
         return null
-      }
-      if (payload.type === 'event_callback') {
+      } else if (payload.type === 'event_callback') {
         if (payload.event.type === 'message') {
           this.messageStore[payload.event.open_message_id] = payload.event
           this.emit('message', {
@@ -104,19 +103,19 @@ class PuppetLark extends Puppet {
         }
         return null
       } else {
-        console.info('Type undefined: ' + payload)
+        console.error('Type undefined: ', payload)
+        log.error('Type undefined: ', payload)
         return null
       }
     })
   }
 
   async stop (): Promise<void> {
-    this.localTunnel.close()
-    console.info('Bot is stopped now.')
+    await this.localTunnel.close()
   }
 
   async logout (): Promise<void> {
-    console.info('WARNNING: There is no need to use this method \'logout\' in a lark bot.')
+    log.warn('There is no need to use this method \'logout\' in a lark bot.')
   }
 
   ding (data?: string): void {
@@ -126,31 +125,25 @@ class PuppetLark extends Puppet {
     this.emit('dong', eventDongPayload)
   }
 
-  contactPhone (contactId: string, phoneList: string[]): Promise<void> {
-    console.info(contactId)
-    console.info(phoneList)
+  contactPhone (): Promise<void> {
     throw new Error('Method not implemented.')
   }
 
-  contactCorporationRemark (contactId: string, corporationRemark: string): Promise<void> {
-    console.info(contactId)
-    console.info(corporationRemark)
+  contactCorporationRemark (): Promise<void> {
     throw new Error('Method not implemented.')
   }
 
-  contactDescription (contactId: string, description: string): Promise<void> {
-    console.info(contactId)
-    console.info(description)
+  contactDescription (): Promise<void> {
     throw new Error('Method not implemented.')
   }
 
-  async contactSelfName (name: string): Promise<void> {
-    console.info(name)
-    console.info('ERROR: The name of lark bot can not be modified.')
+  async contactSelfName (): Promise<void> {
+    console.error('The name of lark bot can not be modified.')
+    log.error('The name of lark bot can not be modified.')
   }
 
   async contactSelfQRCode (): Promise<string> {
-    console.info('A lark bot don\'t have the QR code. So we will show you the bot\'s id.')
+    log.info('A lark bot don\'t have the QR code. So Wechaty will show the bot\'s id.')
     const _token = await this.getTenantAccessToken(this.appId, this.appSecret)
     const response = await axios({
       headers: {
@@ -162,14 +155,14 @@ class PuppetLark extends Puppet {
     if (response.data.code === 0) {
       return response.data.bot.open_id
     } else {
-      console.info(response.data)
+      log.verbose('PuppetLark', 'contactSelfQRCode', response.data)
       return ''
     }
   }
 
-  async contactSelfSignature (signature: string): Promise<void> {
-    console.info(signature)
-    console.info('ERROR: The signature of lark bot can not be modified.')
+  async contactSelfSignature (): Promise<void> {
+    console.error('The signature of lark bot can not be modified.')
+    log.error('The signature of lark bot can not be modified.')
   }
 
   // Tag is use as department in lark bot
@@ -203,9 +196,9 @@ class PuppetLark extends Puppet {
       url: 'https://open.feishu.cn/open-apis/contact/v1/user/update',
     })
     if (response.data.code === '0') {
-      console.info('Successfully modify department.')
+      log.verbose('PuppetLark', 'tagContactAdd', 'Successfully modify department.')
     } else {
-      console.info('Failed to modify department:' + response.data.msg)
+      log.verbose('PuppetLark', 'tagContactAdd', response.data)
     }
   }
 
@@ -224,16 +217,15 @@ class PuppetLark extends Puppet {
       url: 'https://open.feishu.cn/open-apis/contact/v1/user/update',
     })
     if (response.data.code === '0') {
-      console.info('Successfully delete department.')
+      log.verbose('PuppetLark', 'tagContactAdd', 'Successfully delete department.')
     } else {
-      console.info('Failed to delete department:' + response.data.msg)
+      log.verbose('PuppetLark', 'tagContactAdd', 'Failed to delete department:', response.data.msg)
     }
   }
 
   tagContactList(tagId: string, contactId: string): Promise<string[]>;
   tagContactList(): Promise<string[]>
-  tagContactList (contactId?: any): Promise<string[]> | null {
-    console.info(contactId)
+  tagContactList (): Promise<string[]> | null {
     throw new Error('Method not implemented.')
   }
 
@@ -253,26 +245,24 @@ class PuppetLark extends Puppet {
       url: 'https://open.feishu.cn/open-apis/contact/v1/user/update',
     })
     if (response.data.code === '0') {
-      console.info('Successfully modify department.')
+      log.verbose('PuppetLark', 'tagContactRemove', 'Successfully modify department.')
     } else {
-      console.info('Failed to modify department:' + response.data.msg)
+      log.verbose('PuppetLark', 'tagContactRemove', 'Failed to modify department:', response.data.msg)
     }
   }
 
   contactAlias(contactId: string): Promise<string>
   contactAlias(contactId: string, alias: string): Promise<void>
-  async contactAlias (contactId: any, alias?: any): Promise<string | void> {
-    console.info(contactId)
-    console.info(alias)
-    console.info('ERROR: There is no alias in lark.')
+  async contactAlias (): Promise<string | void> {
+    console.error('There is no alias in lark.')
+    log.error('There is no alias in lark.')
   }
 
   contactAvatar(contactId: string): Promise<FileBox>
   contactAvatar(contactId: string, file: FileBox): Promise<void>
-  async contactAvatar (contactId: any, file?: any): Promise<FileBox | void> {
-    console.info(contactId)
-    console.info(file)
-    console.info('ERROR: The avatar of lark contact can not be modified.')
+  async contactAvatar (): Promise<FileBox | void> {
+    console.error('The avatar of lark contact can not be modified.')
+    log.error('The avatar of lark contact can not be modified.')
   }
 
   async contactList (): Promise<string[]> {
@@ -290,35 +280,31 @@ class PuppetLark extends Puppet {
       authedEmployee = await this.getEmployeeList(authedEmployeeIdList)
       return authedEmployee
     } else {
-      console.info(response)
       return authedEmployee
     }
   }
 
-  protected contactRawPayload (contactId: string): Promise<any> {
-    console.info(contactId)
+  protected contactRawPayload (): Promise<any> {
     throw new Error('Method not implemented.')
   }
 
-  protected contactRawPayloadParser (rawPayload: any): Promise<import('wechaty-puppet').ContactPayload> {
-    console.info(rawPayload)
+  protected contactRawPayloadParser (): Promise<import('wechaty-puppet').ContactPayload> {
     throw new Error('Method not implemented.')
   }
 
-  async friendshipAccept (friendshipId: string): Promise<void> {
-    console.info(friendshipId)
-    console.info('ERROR: There is no need to use this method \'friendshipAccept\' in lark.')
+  async friendshipAccept (): Promise<void> {
+    console.error('There is no need to use this method \'friendshipAccept\' in lark.')
+    log.error('There is no need to use this method \'friendshipAccept\' in lark.')
   }
 
-  async friendshipAdd (contactId: string, hello?: string): Promise<void> {
-    console.info(contactId)
-    console.info(hello)
-    console.info('ERROR: There is no need to use this method \'friendshipAdd\' in lark.')
+  async friendshipAdd (): Promise<void> {
+    console.error('There is no need to use this method \'friendshipAdd\' in lark.')
+    log.error('There is no need to use this method \'friendshipAdd\' in lark.')
   }
 
-  async friendshipSearchPhone (phone: string): Promise<string> {
-    console.info(phone)
-    console.info('ERROR: This method \'friendshipSearchPhone\' is not avilable now.')
+  async friendshipSearchPhone (): Promise<string> {
+    console.error('This method \'friendshipSearchPhone\' is not avilable now.')
+    log.error('This method \'friendshipSearchPhone\' is not avilable now.')
     return ''
     // let _token = await this.getTenantAccessToken(this.appId, this.appSecret)
     // let response = await axios({
@@ -341,24 +327,21 @@ class PuppetLark extends Puppet {
     // }
   }
 
-  async friendshipSearchWeixin (weixin: string): Promise<string> {
-    console.info(weixin)
-    console.info('ERROR: Method \'friendshipSearchWeixin\' is not available in lark.')
+  async friendshipSearchWeixin (): Promise<string> {
+    console.error('Method \'friendshipSearchWeixin\' is not available in lark.')
+    log.error('Method \'friendshipSearchWeixin\' is not available in lark.')
     return ''
   }
 
-  protected friendshipRawPayload (friendshipId: string): Promise<any> {
-    console.info(friendshipId)
+  protected friendshipRawPayload (): Promise<any> {
     throw new Error('Method not implemented.')
   }
 
-  protected friendshipRawPayloadParser (rawPayload: any): Promise<import('wechaty-puppet').FriendshipPayload> {
-    console.info(rawPayload)
+  protected friendshipRawPayloadParser (): Promise<import('wechaty-puppet').FriendshipPayload> {
     throw new Error('Method not implemented.')
   }
 
-  async messageContact (messageId: string): Promise<string> {
-    console.info(messageId)
+  async messageContact (): Promise<string> {
     throw new Error('Method not implemented.')
   }
 
@@ -400,20 +383,17 @@ class PuppetLark extends Puppet {
     return file
   }
 
-  messageMiniProgram (messageId: string): Promise<MiniProgramPayload> {
-    console.info(messageId)
+  messageMiniProgram (): Promise<MiniProgramPayload> {
     throw new Error('Method not implemented.')
   }
 
-  messageUrl (messageId: string): Promise<UrlLinkPayload> {
-    console.info(messageId)
+  messageUrl (): Promise<UrlLinkPayload> {
     throw new Error('Method not implemented.')
   }
 
-  async messageSendContact (conversationId: string, contactId: string): Promise<string | void> {
-    console.info(conversationId)
-    console.info(contactId)
-    console.info('ERROR: You can not send contact with bot in lark yet.')
+  async messageSendContact (): Promise<string | void> {
+    console.error('You can not send contact with bot in lark yet.')
+    log.error('You can not send contact with bot in lark yet.')
   }
 
   async messageSendFile (conversationId: string, file: FileBox): Promise<string | void> {
@@ -438,17 +418,15 @@ class PuppetLark extends Puppet {
         url: 'https://open.feishu.cn/open-apis/message/v4/send/',
       })
       if (response.data.code === '0') {
-        console.info('Successfully send image.')
+        log.verbose('PuppetLark', 'messageSendFile', 'Successfully send image.')
       } else {
-        console.info('ERROR: Failed to send image:' + response.data.msg)
+        log.error('Failed to send image:', response.data.msg)
       }
     }
   }
 
-  async messageSendMiniProgram (conversationId: string, miniProgramPayload: MiniProgramPayload): Promise<string | void> {
-    console.info(conversationId)
-    console.info(miniProgramPayload)
-    console.info('ERROR: There is no mini program in lark.')
+  async messageSendMiniProgram (): Promise<string | void> {
+    log.error('There is no mini program in lark.')
   }
 
   async messageSendText (conversationId: string, text: string): Promise<string | void> {
@@ -491,8 +469,7 @@ class PuppetLark extends Puppet {
     })
   }
 
-  messageRecall (messageId: string): Promise<boolean> {
-    console.info(messageId)
+  messageRecall (): Promise<boolean> {
     throw new Error('Method not implemented.')
   }
 
@@ -519,7 +496,7 @@ class PuppetLark extends Puppet {
   }
 
   async roomInvitationAccept (roomInvitationId: string): Promise<void> {
-    console.info('WARNNING: This methods \'roomInvitationAccept\' is used to invite bot into room in lark.')
+    log.warn('This methods \'roomInvitationAccept\' is used to invite bot into room in lark.')
     const _token = await this.getTenantAccessToken(this.appId, this.appSecret)
     await axios({
       data:
@@ -535,13 +512,11 @@ class PuppetLark extends Puppet {
     })
   }
 
-  protected roomInvitationRawPayload (roomInvitationId: string): Promise<any> {
-    console.info(roomInvitationId)
+  protected roomInvitationRawPayload (): Promise<any> {
     throw new Error('Method not implemented.')
   }
 
-  protected roomInvitationRawPayloadParser (rawPayload: any): Promise<import('wechaty-puppet').RoomInvitationPayload> {
-    console.info(rawPayload)
+  protected roomInvitationRawPayloadParser (): Promise<import('wechaty-puppet').RoomInvitationPayload> {
     throw new Error('Method not implemented.')
   }
 
@@ -562,9 +537,8 @@ class PuppetLark extends Puppet {
     })
   }
 
-  async roomAvatar (roomId: string): Promise<FileBox> {
-    console.info(roomId)
-    console.info('You can not get room avatar with lark bot yet.')
+  async roomAvatar (): Promise<FileBox> {
+    log.warn('You can not get room avatar with lark bot yet.')
     return FileBox.fromUrl('')
   }
 
@@ -584,9 +558,9 @@ class PuppetLark extends Puppet {
       url: 'https://open.feishu.cn/open-apis/chat/v4/create/',
     })
     if (response.data.code === 0) {
-      console.info('Successfully create room!')
+      log.verbose('PuppetLark', 'roomCreate', 'Successfully create room!')
     } else {
-      console.info(response.data)
+      log.verbose('PuppetLark', 'roomCreate', 'Failed create room:', response.data.msg)
     }
     return response.data.chat_id
 
@@ -608,9 +582,9 @@ class PuppetLark extends Puppet {
       url: 'https://open.feishu.cn/open-apis/chat/v4/chatter/delete/',
     })
     if (response.data.code === 0) {
-      console.info('Successfully remove user!')
+      log.verbose('PuppetLark', 'roomDel', 'Successfully remove user!')
     } else {
-      console.info(response.data)
+      log.verbose('PuppetLark', 'roomDel', 'Failed remove user:', response.data.msg)
     }
   }
 
@@ -642,16 +616,16 @@ class PuppetLark extends Puppet {
       }
     }
     if (response.data.code === 0) {
-      console.info('Successfully get room list!')
+      log.verbose('PuppetLark', 'roomList', 'Successfully get room list!')
     } else {
-      console.info(response.data)
+      log.verbose('PuppetLark', 'roomList', 'Failed get room list:', response.data.msg)
     }
     return results
   }
 
-  async roomQRCode (roomId: string): Promise<string> {
-    console.info(roomId)
-    console.info('ERROR: You can not get QR code in lark.')
+  async roomQRCode (): Promise<string> {
+    console.error('You can not get QR code in lark.')
+    log.error('You can not get QR code in lark.')
     return ''
   }
 
@@ -670,9 +644,9 @@ class PuppetLark extends Puppet {
       url: 'https://open.feishu.cn/open-apis/bot/v4/remove',
     })
     if (response.data.code === 0) {
-      console.info('Successfully quit room')
+      log.verbose('PuppetLark', 'roomQuit', 'Successfully quit room')
     } else {
-      console.info(response.data.msg)
+      log.verbose('PuppetLark', 'roomQuit', 'Failed quit room:', response.data.msg)
     }
   }
 
@@ -681,7 +655,7 @@ class PuppetLark extends Puppet {
   async roomTopic (roomId: any, topic?: any): Promise<any> {
     if (topic != null) {
       const _token = await this.getTenantAccessToken(this.appId, this.appSecret)
-      const response = await axios({
+      await axios({
         data: {
           chat_id: roomId,
           name: topic,
@@ -693,7 +667,6 @@ class PuppetLark extends Puppet {
         method: 'POST',
         url: 'https://open.feishu.cn/open-apis/chat/v4/update/',
       })
-      console.info(response.data)
     } else {
       const _token = await this.getTenantAccessToken(this.appId, this.appSecret)
       const response = await axios({
@@ -711,22 +684,19 @@ class PuppetLark extends Puppet {
 
   }
 
-  protected roomRawPayload (roomId: string): Promise<any> {
-    console.info(roomId)
+  protected roomRawPayload (): Promise<any> {
     throw new Error('Method not implemented.')
   }
 
-  protected roomRawPayloadParser (rawPayload: any): Promise<import('wechaty-puppet').RoomPayload> {
-    console.info(rawPayload)
+  protected roomRawPayloadParser (): Promise<import('wechaty-puppet').RoomPayload> {
     throw new Error('Method not implemented.')
   }
 
   roomAnnounce(roomId: string): Promise<string>
   roomAnnounce(roomId: string, text: string): Promise<void>
-  async roomAnnounce (roomId: any, text?: any): Promise<string | void> {
-    console.info(roomId)
-    console.info(text)
-    console.info('ERROR: You can not send room announce in lark.')
+  async roomAnnounce (): Promise<string | void> {
+    console.error('You can not send room announce in lark.')
+    log.error('You can not send room announce in lark.')
   }
 
   async roomMemberList (roomId: string): Promise<string[]> {
@@ -744,14 +714,11 @@ class PuppetLark extends Puppet {
     return response.data.data.members
   }
 
-  protected roomMemberRawPayload (roomId: string, contactId: string): Promise<any> {
-    console.info(roomId)
-    console.info(contactId)
+  protected roomMemberRawPayload (): Promise<any> {
     throw new Error('Method not implemented.')
   }
 
-  protected roomMemberRawPayloadParser (rawPayload: any): Promise<import('wechaty-puppet').RoomMemberPayload> {
-    console.info(rawPayload)
+  protected roomMemberRawPayloadParser (): Promise<import('wechaty-puppet').RoomMemberPayload> {
     throw new Error('Method not implemented.')
   }
 
@@ -790,7 +757,6 @@ class PuppetLark extends Puppet {
     if (response.data.code === 0) {
       return response.data.data.image_key
     } else {
-      console.info(response.data)
       return ''
     }
   }
